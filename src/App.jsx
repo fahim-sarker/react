042f84ceaa668a6b490f02b01a282df4ca87a716
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-03-14 01:49:30 +0600

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
-import Banner from './components/banner/Banner'
-import Menu from './components/menu/Menu'
+import { useState } from "react"
+import "./index.css"
 
 function App() {
+  let [ami,setAmi] = useState(0)
+  let [show,setShow] = useState(false)
+  let handleclickone = ()=>{
+    setShow(!show)
+  }
+  let handleincrement = () =>{
+    if(ami < 10){
+      setAmi(ami +1)
+    }
+  }
+  let handledecrement = () =>{
+    if(ami >0){
+      setAmi(ami -1)
+    }
+  }
   return (
     <>
-    <Menu/>
-    <Banner/>
+      <button className="click" onClick={handleclickone}>click</button>
+      {show == true &&<div className="main">
+      <button onClick={handledecrement}>-</button>
+      <h2>{ami}</h2>
+      <button onClick={handleincrement}>+</button>
+    </div>}
     </>
   )
 }
